b72499ec0f30d7c7808732f063bfb04b79ccf332
marisarze <flypetrov@yandex.ru>
2022-10-20 15:47:34 +0300

    change 1

--- main.cpp
+++ main.cpp
@@ -2,7 +2,8 @@
 #include <iostream>
 #include <memory>
 #include <fmt/core.h>
-
+#include <cstdint>
+#include <bitset>
 #define UNUSED(variable) (void)variable
 
 int main(int argc, char const *argv[])
@@ -14,8 +15,10 @@ int main(int argc, char const *argv[])
         int x = 635;
         int* ptr = &x;
         std::cout << ptr << std::endl;
-        const auto ptr_bits = std::bit_cast<uintptr_t>(ptr);
+        const auto ptr_bits = reinterpret_cast<uintptr_t>(ptr);
         std::bitset<32> bits(ptr_bits);
+        std::cout << bits << std::endl;
+        
         // int a = 1;
         // std::cout << "Hello world" << std::endl;
         // fmt::print("Hello from fmt {}", a);
